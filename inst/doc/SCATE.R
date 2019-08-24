3d2eb43f3f7da431b14125f05e69b6e521fc16a5
Zhicheng Ji <zhichengji@gmail.com>
2019-08-24 16:17:51 -0400

    add make database

--- inst/doc/SCATE.R
+++ inst/doc/SCATE.R
@@ -1,34 +1,24 @@
 ## ------------------------------------------------------------------------
-suppressWarnings(suppressMessages(library(SCATE)))
+suppressMessages(library(SCATE))
 set.seed(12345)
-bamlist <- list.files(paste0(system.file(package="SCATE")
-,"/extdata/example"),full.names = T)
+bamlist <- list.files(paste0(system.file(package="SCATE"),"/extdata/example"),full.names = T)
 satac <- sapply(sapply(bamlist,readGAlignmentPairs),GRanges)
 
 ## ------------------------------------------------------------------------
-suppressWarnings(satac <- satacprocess(satac))
+satac <- satacprocess(satac)
 
 ## ------------------------------------------------------------------------
-cellclu <- cellcluster(satac,genome="hg19",clunum=2,perplexity=5)
+cellclu <- cellcluster(satac,genome="hg19",perplexity=5)
 
 ## ------------------------------------------------------------------------
 library(ggplot2)
-plotdata <- data.frame(tSNE1=cellclu$tsne[,1],tSNE2=cellclu$tsne[,2],
-Cluster=as.factor(cellclu$cluster))
-ggplot(plotdata,aes(x=tSNE1,y=tSNE2,col=Cluster)) + geom_point()
+ggplot(data.frame(tSNE1=cellclu$tsne[,1],tSNE2=cellclu$tsne[,2],Cluster=as.factor(cellclu$cluster)),aes(x=tSNE1,y=tSNE2,col=Cluster)) + geom_point() + theme_classic()
 
 ## ------------------------------------------------------------------------
 SCATEres <- SCATE(satac[cellclu$cluster==1],genome="hg19")
 summary(SCATEres)
 
-## ----eval=FALSE----------------------------------------------------------
-#  cellcluster <- head(names(satac),n=3)
-#  SCATEres <- SCATE(satac[cellcluster],genome="hg19")
-
 ## ------------------------------------------------------------------------
 peakres <- peakcall(SCATEres,genome="hg19")
 peakres[[1]]
-
-## ----eval=FALSE----------------------------------------------------------
-#  SCATEpipeline(bamlist,genome="hg19",perplexity=5)
 
